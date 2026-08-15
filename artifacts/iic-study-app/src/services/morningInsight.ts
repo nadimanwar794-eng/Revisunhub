4591aa490e48d3e9d2060faaf677889f15cb602d
nadim anwar <nadimanwar794@gmail.com>
2026-08-15 12:09:28 +0000

    Unzipped code and updated

--- artifacts/iic-study-app/src/services/morningInsight.ts
+++
@@ -1,5 +0,0 @@
-import { UniversalAnalysisLog } from '../types';
-
-export const generateMorningInsight = async (_logs: UniversalAnalysisLog[], _settings: any, _onSave: (banner: any) => void): Promise<string> => {
-    return "AI insight generation is currently disabled.";
-};
--- start.sh
+++
@@ -1,3 +0,0 @@
-cd artifacts/iic-study-app
-pnpm run dev &
-echo $! > server.pid
